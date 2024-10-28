9505c58f28f317fa41fdd479a9c3de6f2f704bb8
GustavoHSS1 <mrpoteishere@gmail.com>
2024-10-28 14:33:25 -0300

    js

---
+++ Fundamentos-do-JavaScript/javaScript/exercicios/ConversorDeMedida/index.js
@@ -0,0 +1,8 @@
+const value = prompt("Digite um valor em metros:\nmm)\ncm)\ndm)\ndam)\nhm)\nkm")
+
+const valueNumerico = parseFloat(value)
+
+switch (valueNumerico) {
+    case 1:
+        alert("o valor é ")
+}
