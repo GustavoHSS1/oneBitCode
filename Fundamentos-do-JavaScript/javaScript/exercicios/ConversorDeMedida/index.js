5ebf6afa5edfba46f643e581f804a8dbfe86fbdd
GustavoHSS1 <mrpoteishere@gmail.com>
2024-10-28 15:00:58 -0300

    js

--- Fundamentos-do-JavaScript/javaScript/exercicios/ConversorDeMedida/index.js
+++ Fundamentos-do-JavaScript/javaScript/exercicios/ConversorDeMedida/index.js
@@ -1,8 +1,33 @@
-const value = prompt("Digite um valor em metros:\nmm)\ncm)\ndm)\ndam)\nhm)\nkm")
+const medida = prompt("Digite um valor em metros:")
 
-const valueNumerico = parseFloat(value)
+const unidade = prompt("Para qual unidade de medidade deseja converter" +
+    "\n1. milimetros (mm)" +
+    "\n2. centímetros (cm)" +
+    "\n3. decímetros (dm)" +
+    "\n4. decâmetros (dam)" +
+    "\n5. hectômetros (hm)" +
+    "\n6. quilômetros (km)"
+)
 
-switch (valueNumerico) {
-    case 1:
-        alert("o valor é ")
+switch (unidade) {
+    case "1": 
+        alert("Resultado " + medida + "m = " + medida * 1000 + "mm")
+        break
+    case "2":
+        alert("Resultado " + medida + "m = " + medida * 100 + "cm")
+        break
+    case "3":
+        alert("Resultado " + medida + "m = " + medida * 10 + "dm")
+        break
+    case "4":
+        alert("Resultado " + medida + "m = " + medida / 10 + "dam")
+        break
+    case "5":
+        alert("Resultado " + medida + "m = " + medida / 100 + "hm")
+        break
+    case "6": 
+    alert("Resultado " + medida + "m = " + medida / 1000 + "km")
+    break
+    default:
+        alert("Opção inválida")
 }
